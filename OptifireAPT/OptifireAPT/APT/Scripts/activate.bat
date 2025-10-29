88fe3840f8688697d783d40dddc8126d40b29737
HectorMP <hect.morales@duocuc.cl>
2025-10-29 00:34:08 -0300

    vista de tarjetas de información con css

--- OptifireAPT/OptifireAPT/APT/Scripts/activate.bat
+++ OptifireAPT/OptifireAPT/APT/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\POANBA02CW1312\Desktop\Proyecto_APT122\OptifireAPT\OptifireAPT\APT
+set VIRTUAL_ENV=C:\Users\hemor\OneDrive\Escritorio\Documentos Duoc\Proyecto_APT122\OptifireAPT\OptifireAPT\APT
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(APT) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(APT) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(APT) 
+set VIRTUAL_ENV_PROMPT=APT
 
 :END
 if defined _OLD_CODEPAGE (
