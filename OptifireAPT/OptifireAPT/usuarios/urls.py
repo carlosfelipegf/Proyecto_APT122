d1191efd7273bc396efc2ec5fcc9af3ed7868ee5
diego <die.marinf@duocuc.cl>
2025-12-01 22:38:16 -0300

    Restaurar Contraseña Adherido

--- OptifireAPT/OptifireAPT/usuarios/urls.py
+++ OptifireAPT/OptifireAPT/usuarios/urls.py
@@ -1,4 +1,6 @@
+
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from .views import CambioContrasenaForzadoView 
 
@@ -12,6 +14,12 @@ urlpatterns = [
     path('nosotros/', views.nosotros_view, name='nosotros'),
     path('notificacion/leida/<int:pk>/', views.marcar_notificacion_leida, name='marcar_notificacion_leida'),
     path('seguridad/cambiar-password/', CambioContrasenaForzadoView.as_view(), name='cambiar_password_forzado'),
+
+    # --- Recuperación de contraseña ---
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
     # =========================================
     # ROUTER CENTRAL (DASHBOARD)
     # =========================================
